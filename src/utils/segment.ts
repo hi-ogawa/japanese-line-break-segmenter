8df55591a7aa9018f24bde5c7af1c32e29065e07
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-10-01 14:06:20 +0900

    ci: setup github workflows

--- src/utils/segment.ts
+++ src/utils/segment.ts
@@ -5,3 +5,25 @@ export function segmentTokens(tokens: Token[]): Token[][] {
   tokens;
   return [];
 }
+
+// ideas are based on https://github.com/google/budou/blob/d45791a244e00d84f87da2a4678da2b63a9c232f/budou/mecabsegmenter.py#L95-L108
+
+// const DEPENDENT_POS_FORWARD = ["接頭辞", "連体詞"];
+// const DEPENDENT_POS_BACKWARD = ["助詞", "助動詞", "接尾辞"];
+// const DEPENDENT_POS_PAIRS = [["名詞", "名詞"]];
+
+// // https://en.wikipedia.org/wiki/Unicode_character_property
+// const PUNCTUATION_OPEN = ["Ps", "Pi"];
+// const PUNCTUATION_CLOSE = ["Pc", "Pd", "Pd", "Pe", "Pf", "Po"];
+
+// function isDependentForward(token: Token): boolean {
+//   return false;
+// }
+
+// function isDependentBackward(token: Token): boolean {
+//   return false;
+// }
+
+// function isDependentPair(prev: Token, curr: Token): boolean {
+//   return false;
+// }
